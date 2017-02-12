4cea37efac1bd70ca57e114dc47fb682962f0288
Ajibade Dahunsi <ajibade@ajibades-mbp.attlocal.net>
2017-02-12 10:54:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,32 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ajibade on 2/10/17.
+ */
+
+public final class PetContract {
+
+    private PetContract() {}
+
+    public static final class PetEntry implements BaseColumns{
+        public final static String TABLE_NAME = "pets";
+
+        /**
+         * Unique ID number for the pet (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME ="name";
+        public final static String COLUMN_PET_BREED ="breed";
+        public final static String COLUMN_PET_GENDER ="gender";
+        public final static String COLUMN_PET_WEIGHT ="weight";
+
+        public final static int GENDER_UNKNOWN = 0;
+        public final static int GENDER_MALE = 1;
+        public final static int GENDER_FEMALE = 2;
+
+    }
+}
